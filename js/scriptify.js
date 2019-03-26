f6152f8c76efe675425a443820ceaae8b2d3789d
gotylergo <tk03759@gmail.com>
2019-03-25 23:49:35 -0700

    Detect IE and warn user of incompatability

--- js/scriptify.js
+++ js/scriptify.js
@@ -1,7 +1,7 @@
 // This array is used to create the checkboxes in the html
 
 let allPackages = [
-    'powershell', 
+    'powershell',
     '7zip',
     'adobereader',
     'evernote',
@@ -30,29 +30,40 @@ let templates = {
         'adobereader',
         'evernote',
         'libreoffice-fresh',
-        ],
+    ],
 }
 
 // Set a VPN client package to install
 
 let vpn = 'openvpn'
 
+// Warn IE users to use a modern browser
+
+const usingIE = /Trident|MSIE/.test(navigator.userAgent);
+
+if (usingIE) {
+    document.getElementById("ifIE").style.display = "block";
+} else {
+    document.getElementById("ifIE").style.display = "none";
+}
+
+
 // Create template selections from templates array
 
-let templateOption = function(templateName) {
-    return(`<option value="${templateName}">${templateName}</option>`)
+let templateOption = function (templateName) {
+    return (`<option value="${templateName}">${templateName}</option>`)
 }
 
 for (let template in templates) {
-    if(templates.hasOwnProperty(template)) {
+    if (templates.hasOwnProperty(template)) {
         document.getElementById('template').innerHTML += templateOption(template);
     }
 }
 
 // Create checkboxes from allPackages array
 
-let packageCheckbox = function(packageName) {
-    return(`<div class="checkbox">
+let packageCheckbox = function (packageName) {
+    return (`<div class="checkbox">
 <label for="${packageName}"><input class="selections" type="checkbox" id="${packageName}" value="${packageName}">
 ${packageName}</label>
 </div>`)
@@ -64,7 +75,7 @@ for (let pkg in allPackages) {
 
 // Tick of checkboxes when a template is selected
 
-const clearCheckboxes = function() {
+const clearCheckboxes = function () {
     var items = document.getElementsByClassName('selections');
     for (var i = 0; i < items.length; i++) {
         if (items[i].type == 'checkbox')
@@ -101,7 +112,7 @@ form.addEventListener('submit', function (e) {
     let chocoInstaller = `@"%SystemRoot%\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\\chocolatey\\bin" && set choco=%ALLUSERSPROFILE%\\chocolatey\\bin\\choco.exe`;
     let chocoInstall = 'choco install';
     let packageList = '';
-    let condExec = '&&' 
+    let condExec = '&&'
     let script = '';
 
     let inputElements = document.getElementsByClassName('selections');
@@ -115,7 +126,7 @@ form.addEventListener('submit', function (e) {
     if (!installChoco) {
         chocoInstaller = '';
         condExec = ''
-    } 
+    }
 
     if (isLaptop) {
         packageList = `${vpn} ${packageList}`;
